1965cb6b5480e19fc7244f7bd85ebc6ce7599675
Karl M. Davis <karl@justdavis.com>
2013-09-21 21:48:04 -0400

    Updated the bareint package comment to explain more about the package's intent.

--- jversion-sanity/src/main/java/com/justdavis/jessentials/jversionsanity/bareint/package-info.java
+++ jversion-sanity/src/main/java/com/justdavis/jessentials/jversionsanity/bareint/package-info.java
@@ -1,6 +1,7 @@
 /**
  * Classes in this package provide support for 
  * {@link com.justdavis.jessentials.jversionsanity.bareint.IntegerVersion}: a versioning scheme 
- * that only allows for "bare" integers.
+ * that only allows for "bare" integers. This is admittedly a mostly-but-not-entirely useless
+ * implementation: it's intended primarily as a simple way to validate the overall API.
  */
 package com.justdavis.jessentials.jversionsanity.bareint;
