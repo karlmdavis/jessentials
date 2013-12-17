76d89103c58182c8637d9e23cf44666938ebc099
Karl M. Davis <karl@justdavis.com>
2013-12-17 01:09:03 -0500

    Created the DataSourceConnectorsManager helper class.
    It's easier to inject this than the Set<IData...>.

---
+++ jessentials-misc/src/main/java/com/justdavis/karl/misc/datasources/DataSourceConnectorsManager.java
@@ -0,0 +1,119 @@
+package com.justdavis.karl.misc.datasources;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.springframework.stereotype.Component;
+
+import com.justdavis.karl.misc.exceptions.BadCodeMonkeyException;
+import com.justdavis.karl.misc.exceptions.unchecked.UncheckedSqlException;
+
+/**
+ * A helper class for working with an application's {@link IDataSourceConnector}
+ * implementations.
+ */
+@Component
+public final class DataSourceConnectorsManager {
+	private final Set<IDataSourceConnector<? extends IDataSourceCoordinates>> dataSourceConnectors;
+
+	/**
+	 * Constructs a new {@link DataSourceConnectorsManager} instance.
+	 * 
+	 * @param dataSourceConnectors
+	 *            the {@link Set} of {@link IDataSourceConnector}s available to
+	 *            the application
+	 */
+	@Inject
+	public DataSourceConnectorsManager(
+			Set<IDataSourceConnector<? extends IDataSourceCoordinates>> dataSourceConnectors) {
+		/*
+		 * Sanity check: if no IDataSourceConnector implementations are
+		 * registered, there's a problem.
+		 */
+		if (dataSourceConnectors == null || dataSourceConnectors.isEmpty())
+			throw new BadCodeMonkeyException(
+					String.format(
+							"No %s implementations were provided."
+									+ " This is likely a problem with the DI/Spring configuration.",
+							IDataSourceConnector.class.getSimpleName()));
+
+		this.dataSourceConnectors = dataSourceConnectors;
+	}
+
+	/**
+	 * Constructs a new {@link DataSourceConnectorsManager} instance.
+	 * 
+	 * @param dataSourceConnectors
+	 *            the {@link IDataSourceConnector}s available to the application
+	 */
+	public DataSourceConnectorsManager(
+			IDataSourceConnector<? extends IDataSourceCoordinates>... dataSourceConnectors) {
+		this(
+				new HashSet<IDataSourceConnector<? extends IDataSourceCoordinates>>(
+						Arrays.asList(dataSourceConnectors)));
+	}
+
+	/**
+	 * @return all of the {@link IDataSourceCoordinates} types associated with
+	 *         the available {@link IDataSourceConnector}s
+	 */
+	public Set<Class<? extends IDataSourceCoordinates>> getCoordinatesTypes() {
+		Set<Class<? extends IDataSourceCoordinates>> coordTypes = new HashSet<Class<? extends IDataSourceCoordinates>>();
+		for (IDataSourceConnector<? extends IDataSourceCoordinates> connector : dataSourceConnectors)
+			coordTypes.add(connector.getCoordinatesType());
+
+		return coordTypes;
+	}
+
+	/**
+	 * This method will select the {@link IDataSourceConnector} implementation
+	 * that supports the specified {@link IDataSourceCoordinates} instance and
+	 * return the results of
+	 * {@link IDataSourceConnector#createDataSource(IDataSourceCoordinates)} for
+	 * it.
+	 * 
+	 * @param coords
+	 *            an {@link IDataSourceCoordinates} instance that provides the
+	 *            data/settings needed to create a {@link DataSource} connection
+	 * @return a new JDBC {@link DataSource} instance for the data store
+	 *         represented by the specified {@link IDataSourceCoordinates}
+	 *         instance
+	 * @throws UncheckedSqlException
+	 *             An {@link UncheckedSqlException} may be thrown if any
+	 *             {@link SQLException}s or similar errors are encountered while
+	 *             creating the {@link DataSource} instance.
+	 * @throws IllegalArgumentException
+	 *             an {@link IllegalArgumentException} will be thrown if no
+	 *             matching {@link IDataSourceConnector} can be found for the
+	 *             specified {@link IDataSourceCoordinates}.
+	 */
+	@SuppressWarnings("unchecked")
+	public DataSource createDataSource(IDataSourceCoordinates coords)
+			throws UncheckedSqlException, IllegalArgumentException {
+		// Sanity check: null coords?
+		if (coords == null)
+			throw new IllegalArgumentException();
+
+		// Find the IDataSourceConnector that supports the coords.
+		@SuppressWarnings("rawtypes")
+		IDataSourceConnector matchingConnector = null;
+		for (IDataSourceConnector<? extends IDataSourceCoordinates> connector : dataSourceConnectors)
+			if (connector.getCoordinatesType().isAssignableFrom(
+					coords.getClass()))
+				matchingConnector = connector;
+
+		// Did we find a match?
+		if (matchingConnector == null)
+			throw new IllegalArgumentException(String.format(
+					"No matching %s found for the coordinates of type '%s'.",
+					IDataSourceConnector.class.getSimpleName(),
+					coords.getClass()));
+
+		return matchingConnector.createDataSource(coords);
+	}
+}
---
+++ jessentials-misc/src/test/java/com/justdavis/karl/misc/datasources/DataSourceConnectorsManagerTest.java
@@ -0,0 +1,50 @@
+package com.justdavis.karl.misc.datasources;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.sql.DataSource;
+
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.justdavis.karl.misc.datasources.hsql.HsqlConnector;
+import com.justdavis.karl.misc.datasources.hsql.HsqlCoordinates;
+
+/**
+ * Unit tests for {@link DataSourceConnectorsManager}.
+ */
+public final class DataSourceConnectorsManagerTest {
+	/**
+	 * Ensures that {@link DataSourceConnectorsManager#getCoordinatesTypes()}
+	 * works as expected.
+	 */
+	@Test
+	public void getCoordinatesTypes() {
+		DataSourceConnectorsManager connectorsManager = new DataSourceConnectorsManager(
+				new HashSet<IDataSourceConnector<? extends IDataSourceCoordinates>>(
+						Arrays.asList(new HsqlConnector())));
+
+		Assert.assertEquals(1, connectorsManager.getCoordinatesTypes().size());
+		Assert.assertEquals(HsqlCoordinates.class, connectorsManager
+				.getCoordinatesTypes().iterator().next());
+	}
+
+	/**
+	 * Ensures that
+	 * {@link DataSourceConnectorsManager#createDataSource(IDataSourceCoordinates)}
+	 * works as expected.
+	 */
+	@Test
+	public void createDataSource() {
+		DataSourceConnectorsManager connectorsManager = new DataSourceConnectorsManager(
+				new HashSet<IDataSourceConnector<? extends IDataSourceCoordinates>>(
+						Arrays.asList(new HsqlConnector())));
+
+		DataSource dataSource = connectorsManager
+				.createDataSource(new HsqlCoordinates(
+						"jdbc:hsqldb:mem:foo;shutdown=true"));
+		Assert.assertTrue(dataSource instanceof JDBCDataSource);
+	}
+}
